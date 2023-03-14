1663d48638cafa080f032a3849656fe814f8837d
Pier4413 <pierre.delmas@delmasweb.net>
2023-03-14 23:00:05 +0100

    Ajout des zones d'impact pour les torpilles sur la grille et du choix de l'utilisation ou non du sonar. Ajout de commentaires

--- main3.py
+++ main3.py
@@ -23,29 +23,35 @@ class Box(object):
     self.sector = sector
     self.is_island = is_island
     self.is_start = is_start
-    self.last_turn = 0
   
   def determine_mouvement(self, end_box : "Box"):
+    """
+      Determine si une case est disponible pour etre un bon point d'arrivee ou pas
+    """
     if end_box.is_island is False:
       end_box.is_start = self.is_start
 
   def __str__(self) -> str:
-    ret = f""
-    """
-    if self.is_island:
-      ret = f"{ret}x"
-    else:
-      ret = f"{ret}."
-    """
-    ret = f"{ret}{1 if self.is_start is True else 0}"
-    return ret
+    return f"{1 if self.is_start is True else 0}"
 
 class Row(list[Box]):
   
   def __init__(self):
+    """
+      Constructeur
+
+      Initialise une liste vide pour les cases d'eau uniquement
+    """
     self.list_of_water = list[Box]()
 
   def append(self, __object: "Box") -> None:
+    """
+      Ajoute un element de type box dans la grille
+      Si la case est de l'eau on l'ajoute aussi a la liste des cases d'eau
+
+      :param __object: La boite a ajouter
+      :type __object: Box
+    """
     super().append(__object)
     if __object.is_island is False:
       self.list_of_water.append(__object)
@@ -59,13 +65,30 @@ class Row(list[Box]):
 class Grid(list[Row]):
   
   def __init__(self):
-    self.list_of_water = list[Row]()
+    """
+      Constructeur
+
+      Initialise une liste vide pour les cases d'eau uniquement
+    """
+    self.list_of_water = list[list[Box]]()
 
   def append(self, __object: "Row") -> None:
+    """
+      Ajoute un element de type row dans la grille
+
+      :param __object: La ligne a ajouter
+      :type __object: Row
+    """
     super().append(__object)
     self.list_of_water.append(__object.list_of_water)
 
   def copy(self, other : "Grid"):
+    """
+      Copieur de grille par valeur
+
+      :param other: La grille que l'on veut copier
+      :type other: Grid
+    """
     self.clear()
     for row in other:
       temp_row = Row()
@@ -76,6 +99,9 @@ class Grid(list[Row]):
     return self
 
   def __str__(self) -> str:
+    """
+      Affichage de la grille
+    """
     ret = f"Grid : \n"
 
     for r in self:
@@ -94,35 +120,60 @@ class Player(object):
       :param grid: La grille du jeu attaché au joueur
       :type grid: Grid
     """
-    self.id = id
-    self.x = 0
-    self.y = 0
-    self.grid_start = grid
-    self.grid_end = Grid().copy(grid)
-    self.life = 6
-    self.torpedo = 0
-    self.sonar = 0
-    self.silence = 0
-    self.mine = 0
-    self.coordinate_list = list[Box]()
+    self.id = id # Identifiant du joueur
+    self.x = 0 # Position courante en X
+    self.y = 0 # Position courant en Y
+    self.grid_start = grid # Grille de depart
+    self.grid_end = Grid().copy(grid) # Grille de fin
+    self.life = 6 # Nombre de points de vie au depart
+    self.torpedo = 0 # Nombre de tours avant chargement d'une torpille
+    self.sonar = 0 # Nombre de tours avant chargement du sonar
+    self.silence = 0 # Nombre de tours avant chargement du silence
+    self.mine = 0 # Nombre de tours avant le chargement des mines
+    self.coordinate_list = list[Box]() # Liste des coordonnes des cases deja visites (reinit sur SURFACE)
   
   def calculate_is_possible(self, mouvement : str):
+    """
+      A partir d'un mouvement donne. On calcule toutes les cases de depart possible pour ce
+      mouvement. Le but est de determiner avec une bonne certitude ou peut etre l'ennemi
+
+      A noter si le mouvement est W alors on peut partir du principe que l'ennemi n'est pas dans la premiere colonne
+      De meme pour E et la derniere colonne, pour N et la premiere ligne et pour S et la derniere ligne
+
+      :param mouvement: Le mouvement a analyser
+      :type mouvement: str
+    """
     for row in self.grid_start:
       for box in row:
         if mouvement == "N":
-          if box.y - 1 >= 0:
+          if box.y - 1 > 0:
             box.determine_mouvement(self.grid_end[box.y-1][box.x])
+          if box.y == 0:
+            self.grid_end[box.y][box.x].is_start = False
         elif mouvement == "S":
           if box.y + 1 < k_MapSize:
             box.determine_mouvement(self.grid_end[box.y+1][box.x])
+          if box.y == k_MapSize - 1:
+            self.grid_end[box.y][box.x].is_start = False
         elif mouvement == "E":
           if box.x + 1 < k_MapSize:
             box.determine_mouvement(self.grid_end[box.y][box.x+1])
+          if box.x == k_MapSize - 1:
+            self.grid_end[box.y][box.x].is_start = False
         elif mouvement == "S":
-          if box.x - 1 >= 0:
+          if box.x - 1 > 0:
             box.determine_mouvement(self.grid_end[box.y][box.x-1])
+          if box.x == 0:
+            self.grid_end[box.y][box.x].is_start = False
+    if mouvement == "N":
+      print(f"TEST TEST TEST \n\n{self.grid_end}\n\n")
 
   def get_initial_position(self) -> str:
+    """
+      On choisit la position de départ
+
+      Le choix est random dans une case d'eau
+    """
     l_random_row_index = random.randrange(0,len(self.grid_start) - 1)
     l_random_row = self.grid_start.list_of_water[l_random_row_index]
     l_box_index = random.randrange(0, len(l_random_row) - 1)
@@ -131,7 +182,13 @@ class Player(object):
     return f"{l_box.x} {l_box.y}"
   
   def get_best_move(self) -> str:
+    """
+      Choix du déplacement
 
+      On choisit la case vers laquelle se déplacer. Le but est de se rapprocher de la position probable de
+      l'adversaire
+    """
+    # TODO : Trouver un moyen de se rapprocher de l'adversaire en fonction de la grille des zones de fin (grid_end)
     # On cree la liste des boites voisines
     list_neighbourgs = list[Box]()
 
@@ -172,6 +229,16 @@ class Player(object):
     return "SURFACE"
   
   def get_best_item_to_charge(self) -> str:
+    """
+      Choix du meilleur outil a charger
+
+      Le choix est le suivant :
+
+      - Toujours avoir du SILENCE
+      - Ensuite toujours avoir des MINES
+      - Puis toujours avoir une TORPILLE
+      - Puis enfin avoir du SONAR si possible
+    """
     if self.silence > 0:
       return "SILENCE"
     elif self.mine > 0:
@@ -180,10 +247,53 @@ class Player(object):
       return "TORPEDO"
     elif self.sonar > 0:
       return "SONAR"
-    
     return ""
+  
+  def calculate_torpedo_impact(self, torpedo_coordinates : tuple[int, int]) -> None:
+    """
+      Ameliore la grille de fin a partir des coups de torpille adverses
+    """
+    l_box_impact = self.grid_start[torpedo_coordinates[1]][torpedo_coordinates[0]]
+    for row in self.grid_start:
+      for box in row:
+        if box.x < l_box_impact.x - 4 or box.x > l_box_impact.x + 4 or box.y < l_box_impact.y - 4 or box.y > l_box_impact.y + 4:
+          box.is_start = False
+
+  def calculate_number_of_ones_by_sector(self, sector : int) -> int:
+    """
+      Calcule le nombre de 1 pour un secteur donne
+
+      :param sector: Le secteur que l'on vise
+      :type sector: int
+      :return: Le nombre de 1 dans le secteur
+      :rtype: int
+    """
+    result = 0
+    for y in range(0,k_MapSize):
+      for x in range(0,k_MapSize):
+        if self.grid_end[y][x].sector == sector and self.grid_end[y][x].is_start is True:
+          result += 1
+    return result
+
+  def should_use_sonar(self) -> str:
+    """
+      Determine s'il faut utiliser le sonar et si ou dans quel secteur
+    """
+    if self.sonar == 0:
+      index_of_max = 0
+      value_of_max = 0
+      for i in range(1,10):
+        value = self.calculate_number_of_ones_by_sector(i)
+        if value > value_of_max:
+          value_of_max = value
+          index_of_max = i
+      if index_of_max > 0:
+        return f"SONAR {index_of_max}"
     
   def __str__(self) -> str:
+    """
+      Affiche des informations sur le joueur pour le debug
+    """
     return f"X : {self.x} | Y : {self.y} | Life : {self.life} | Torpedo : {self.torpedo} | Sonar : {self.sonar} | Silence : {self.silence} | Mine : {self.mine}"
   
 k_MapSize = 15
@@ -193,39 +303,58 @@ current_turn = -1
 
 if __name__ == "__main__":
 
+  # On cree le fichier et on l'ouvre si on est en mode Testing
   file = None
   if len(sys.argv) > 1 and sys.argv[1] == "Testing":
     file = open("tester.txt", "r")
 
+  # On lit les dimensions de la carte
   if file is None:
     width, height, my_id = [int(i) for i in input().split()]
   else:
     width, height, my_id = [int(i) for i in file.readline().split()]
+  
+  # On cree nos deux grilles (la notre et celle de l'adversaire)
+  # On a besoin de deux grilles differentes car elles ont vont evoluees differemment en fonction des ordres
   my_grid = Grid()
   opp_grid = Grid()
 
+  # On recupere la taille de la carte et le nombre de cases dans une ligne/colonne de secteur
   k_MapSize = height
   k_GridSector = k_MapSize//k_MapSector
+
+  # On parse la carte ce qui nous permet de remplir nos deux grilles de departs
   for y in range(height):
+
+    # Une ligne de la grille (de meme deux lignes une pour le joueur et une pour l'adversaire)
     my_row = Row()
     opp_row = Row()
 
+    # Suivant si on est dans le fichier ou pas pour le mode testing
     if file is None:
       data = input()
     else:
       data = file.readline()
 
+    # On parse la ligne en cours
     for index, x in enumerate(data):
-      my_row.append(Box(index, y, (index//k_GridSector)+(k_MapSector*(y//k_GridSector))+1, x == "x", x != "x"))
-      opp_row.append(Box(index, y, (index//k_GridSector)+(k_MapSector*(y//k_GridSector))+1, x == "x", x != "x"))
+      # Le numero du secteur en fonction des coordonnees
+      sector = (index//k_GridSector)+(k_MapSector*(y//k_GridSector)) + 1
+      my_row.append(Box(index, y, sector, x == "x", x != "x"))
+      opp_row.append(Box(index, y, sector, x == "x", x != "x"))
     my_grid.append(my_row)
     opp_grid.append(opp_row)
 
+  # On cree les deux joueurs avec leurs id
   my_player = Player(my_id, my_grid)
   opp_player = Player(1-my_id, opp_grid)
 
+  # On choisit la position de depart
   print(my_player.get_initial_position())
+  
   while True:
+
+    # On recupere la liste des inputs (soit depuis le jeu, soit depuis le fichier pour les tests)
     if file is None:
       my_player.x, my_player.y, my_player.life, opp_player.life, my_player.torpedo, my_player.sonar, my_player.silence, my_player.mine = [int(i) for i in input().split()]
       sonar_result = input().split()
@@ -238,11 +367,30 @@ if __name__ == "__main__":
       sonar_result = file.readline().split()
       opponent_orders = file.readline().split("|")
     
-    print(f"{str(my_player)}", file=sys.stderr, flush=True)
+    # On parse les ordres adverses pour detecter la position la plus probable de celui-ci
     for o in opponent_orders:
       if "MOVE" in o:
         mouvement = o.split()[1]
         opp_player.calculate_is_possible(mouvement=mouvement)
+      elif "TORPEDO" in o:
+        case_coordinate = (int(o.split()[1]), int(o.split()[2]))
+        opp_player.calculate_torpedo_impact(torpedo_coordinates=case_coordinate)
+    
 
+    # On copie la grille de fin dans la grille de depart pour le prochain tour
     opp_player.grid_start.copy(opp_player.grid_end)
-    print(f"{my_player.get_best_move()} {my_player.get_best_item_to_charge()}")
+
+
+    # On calcule nos ordres en fonction de la position probable de l'adversaire
+    order = f"{my_player.get_best_move()} {my_player.get_best_item_to_charge()}"
+
+    should_use_torpedo = None # TODO : Implementer l'algo de gestion des tirs de torpille
+    if should_use_torpedo is not None:
+      pass
+    else:
+      should_use_sonar = my_player.should_use_sonar()
+      if should_use_sonar is not None:
+        order = f"{order} | {should_use_sonar}"
+
+    # On envoie l'ordre final
+    print(order)
